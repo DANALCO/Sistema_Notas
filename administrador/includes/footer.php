1b104f37c928be90a11a8180e4ef5d94ed997b51
DANALCO <u20221204296@usco.edu.co>
2024-03-27 15:07:41 -0500

    v1

---
+++ administrador/includes/footer.php
@@ -0,0 +1,13 @@
+    <!-- Essential javascripts for application to work-->
+    <script src="js/jquery-3.7.0.min.js"></script>
+    <script src="js/popper.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+    <script src="js/main.js"></script>
+    <script src="js/plugins/pace.min.js"></script>
+    <!--tablas-->
+    <script type="text/javascript" src="js/plugins/jquery.dataTables.min.js"></script>
+    <script type="text/javascript" src="js/plugins/dataTables.bootstrap.min.js"></script>
+    <script type="text/javascript" src="js/functions-usuarios.js"></script>
+    <!-- Page specific javascripts-->
+  </body>
+</html>
