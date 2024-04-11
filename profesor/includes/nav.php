906c3c419d337f65f1feeddac7677e119cb5d2a4
DANALCO <113032033+DANALCO@users.noreply.github.com>
2024-04-11 15:26:38 -0500

    Add files via upload

---
+++ profesor/includes/nav.php
@@ -0,0 +1,16 @@
+<!-- Sidebar menu-->
+<div class="app-sidebar__overlay" data-toggle="sidebar"></div>
+<aside class="app-sidebar">
+  <div class="app-sidebar__user"><img class="app-sidebar__user-avatar" src="images/admin-user-icon-4.jpg" alt="User Image">
+    <div>
+      <p class="app-sidebar__user-name"><?= $_SESSION['nombre'];?> </p>
+      <p class="app-sidebar__user-designation">Profesor</p>
+    </div>
+  </div>
+  <ul class="app-menu">
+    <li><a class="app-menu__item" href="lista_usuarios.php"><i class="fa-solid fa-users"></i><span class="app-menu__label">Usuarios</span></a></li>
+    <li><a class="app-menu__item" href="lista_profesores.php"><i class="fa-solid fa-chalkboard-user"></i><span class="app-menu__label">Profesores</span></a></li>
+    <li><a class="app-menu__item" href="lista_alumnos.php"><i class="fa-solid fa-child"></i><span class="app-menu__label">Alumnos</span></a></li>
+    <li><a class="app-menu__item" href="../logout.php"><i class="fa-solid fa-right-from-bracket"></i><span class="app-menu__label">Logout</span></a></li>
+  </ul>
+</aside>
