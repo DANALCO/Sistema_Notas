ebccd87b7d06dca01d7c509e93888b1f9b88a962
DANALCO <113032033+DANALCO@users.noreply.github.com>
2024-04-11 15:22:28 -0500

    Add files via upload

---
+++ administrador/index.php
@@ -0,0 +1,17 @@
+<?php
+require_once 'includes/header.php'
+?>
+
+<main class="app-content">
+  
+  <div class="row">
+    <div class="col-md-12">
+      <div class="col-md-12-edit">
+        <img src="images/school.png" alt="imagen secundaria aftons">
+      </div>
+    </div>
+  </div>
+</main>
+<?php
+require_once 'includes/footer.php'
+?>
